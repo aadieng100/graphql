0f60341cf768cf305a208020cd58e12e95701464
abdouladieng <diengabdoulaziz110@gmail.com>
2024-03-28 17:20:27 +0000

    adding modify title

--- static/js/tools.js
+++ static/js/tools.js
@@ -300,7 +300,7 @@ function injectData(data) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>GeeksForGeeks</title>
+        <title>Graphql</title>
         <link rel="stylesheet" href="static/css/home.css">
     </head>
     `
